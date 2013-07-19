085b1affbb8040827cd0f2faacd4b45018b83dc2
jtimon <jtimon@monetize.io>
2013-07-19 12:49:30 +0200

    The foundation address is empty until approved

--- foundation/donations/models.py
+++ foundation/donations/models.py
@@ -5,7 +5,7 @@ class Organization(models.Model):
     website = models.URLField()
     short_description = models.CharField(max_length=60)
     long_description = models.CharField(max_length=600)
-    foundation_address = models.CharField(max_length=34)
+    foundation_address = models.CharField(max_length=34, blank=True)
     freicoin_address = models.CharField(max_length=34)
     bitcoin_address = models.CharField(max_length=34)
     email = models.EmailField()
