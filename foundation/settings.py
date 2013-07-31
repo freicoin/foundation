037f0109f58bd39224f1be222be455d10fb54454
jtimon <jtimon@monetize.io>
2013-07-31 12:42:54 +0200

    Add Heroku

--- foundation/settings.py
+++ foundation/settings.py
@@ -171,3 +171,27 @@ RECAPTCHA_PUBLIC_KEY = '6Ldic-USAAAAAD5qn69vpJLtZpH8IpAHMO7RO3Me'
 RECAPTCHA_USE_SSL = True
 #TODO this shouldn't be here
 RECAPTCHA_PRIVATE_KEY = '6Ldic-USAAAAAMwdRfDUYrI18tNMDe_GTrpmUFSw'
+
+# Heroku
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+# End Heroku
