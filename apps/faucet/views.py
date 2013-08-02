289bf0348abd5b0dedc659510b50c3843f816a80
jtimon <jtimon@monetize.io>
2013-08-02 19:11:42 +0200

    Validate ip, address and time (1 day) ...also a sorry template

--- apps/faucet/views.py
+++ apps/faucet/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import Http404, HttpResponseRedirect
+from django.db.models import Q
+
+from datetime import datetime, timedelta
 
 from apps.faucet.models import *
 from apps.faucet import forms
@@ -17,7 +20,20 @@ def faucet(request):
             send = FaucetSend()
             send.frc_address = form.cleaned_data['frc_address']
             send.ip_address = request.META['REMOTE_ADDR']
-            # TODO validate, actually send freicoins and take the real tx id
+
+            enddate = datetime.now() 
+            startdate = enddate - timedelta(days=1)
+
+            conflict_sends = FaucetSend.objects.filter(
+                timestamp__range=[startdate, enddate]).filter(
+                Q(frc_address=send.frc_address) 
+                | Q(ip_address=send.ip_address))
+
+            if ( conflict_sends.count() > 0 ):
+                return render(request, 'sorry.html', 
+                              {'send_list': conflict_sends.order_by('-timestamp')})
+
+            # TODO actually send freicoins and take the real tx id
             send.tx_id = "0dc2d74db5fddd4f2100f4d99090a2a317f94ed6de17066d366d72ede8336ca4"
             send.save()
 
