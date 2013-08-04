313da25e0bda1d95e64aa081e79d0dcbf23dd12f
jtimon <jtimon@monetize.io>
2013-08-05 01:07:28 +0200

    Add real address validation

--- foundation/forms.py
+++ foundation/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.core.validators import validate_email
+from bitcoin.address import BitcoinAddress
 
 class FrcAddressField(forms.CharField):
     def validate(self, value):
@@ -7,5 +8,10 @@ class FrcAddressField(forms.CharField):
         super(FrcAddressField, self).validate(value)
 
         # TODO make a real address validation
-        if len(value) > 0 and not value.startswith('1'):
+        try:
+            addr = BitcoinAddress(value.decode('base58'))
+        except:
+        # possible exceptions:
+        # InvalidAddressError, VersionedPayloadError,
+        # HashChecksumError, InvalidBase58Error
             raise forms.ValidationError("Not a valid address.")
