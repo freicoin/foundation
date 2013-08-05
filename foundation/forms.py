aaa1e32c09f983da4ad4c730a6b72839fe5a9e5d
jtimon <jtimon@monetize.io>
2013-08-05 19:08:37 +0200

    Reference exceptions explicitly and remove unnecessary import

--- foundation/forms.py
+++ foundation/forms.py
@@ -1,17 +1,15 @@
 from django import forms
-from django.core.validators import validate_email
+
 from bitcoin.address import BitcoinAddress
+from bitcoin.errors import *
 
 class FrcAddressField(forms.CharField):
     def validate(self, value):
         # Use the parent's handling of required fields, etc.
         super(FrcAddressField, self).validate(value)
 
-        # TODO make a real address validation
         try:
             addr = BitcoinAddress(value.decode('base58'))
-        except:
-        # possible exceptions:
-        # InvalidAddressError, VersionedPayloadError,
-        # HashChecksumError, InvalidBase58Error
+        except (InvalidAddressError, VersionedPayloadError, 
+                HashChecksumError, InvalidBase58Error):
             raise forms.ValidationError("Not a valid address.")
