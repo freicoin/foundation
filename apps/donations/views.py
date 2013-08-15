89fcbe56e82c866220f9e7325fd4a8080e08a0d5
jtimon <jtimon@monetize.io>
2013-08-15 14:36:00 +0200

    Send HTML email fix #5

--- apps/donations/views.py
+++ apps/donations/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
 from django.http import Http404, HttpResponseRedirect
 
-from django.core.mail import send_mail
-from django.template.loader import get_template
-from django.template import Context
-
 from django.contrib.auth.decorators import login_required
+
+from django.conf import settings
+from foundation import utils
 
 from models import *
 import forms
 
 def organization_list(request):
-    organizations = Organization.objects.exclude(foundation_address__isnull=True).exclude(foundation_address__exact='')
+    organizations = Organization.objects.exclude(foundation_address__isnull=True
+                                                 ).exclude(foundation_address__exact='')
     return render(request, 'organization_list.html', 
                   {'organization_list': organizations})
 
@@ -25,47 +25,37 @@ def organization_detail(request, organization_id=None):
 
 @login_required
 def new_organization(request):
-    try:
-        if request.method == 'POST':
-            form = forms.OrganizationForm(request.POST)
 
-            if form.is_valid():
-                cd = form.cleaned_data
+    if request.method == 'POST':
+        form = forms.OrganizationForm(request.POST)
 
-                org = Organization()
+        if form.is_valid():
+            cd = form.cleaned_data
 
-                org.name = cd['name']
-                org.website = cd['website']
-                org.short_description = cd['short_description']
-                org.long_description = cd['long_description']
-                org.freicoin_address = cd['freicoin_address']
-                org.bitcoin_address = cd['bitcoin_address']
-                org.email = cd['email']
-                org.save()
+            org = Organization()
 
-               #  admin_mail_t = get_template('admin_mail.html')
-               #  admin_mail = admin_mail_t.render(Context({'org': org}))
-               #  send_mail(
-               #      "New organization registration: %s" % org.name,
-               #      admin_mail,
-               #      cd.get('email', 'noreply@freicoin.org'),
-               #      ['admin@freicoin.org'],
-               #      )
+            org.name = cd['name']
+            org.website = cd['website']
+            org.short_description = cd['short_description']
+            org.long_description = cd['long_description']
+            org.freicoin_address = cd['freicoin_address']
+            org.bitcoin_address = cd['bitcoin_address']
+            org.email = cd['email']
+            org.save()
 
-               # org_mail_t = get_template('org_mail.html')
-               #  org_mail = org_mail_t.render(Context({'org': org}))
-               #  send_mail(
-               #      "Thanks for registering your organization !",
-               #      org_mail,
-               #      'noreply@freicoin.org',
-               #      [org.email],
-               #      )
+            context = {'org': org}
+            utils.send_html_mail('admin_mail.html', context, 
+                                 "New organization registration: %s" % org.name, 
+                                 cd.get('email', 'noreply@freicoin.org'),
+                                 settings.DONATION_ADMIN_MAILS)
 
-                return HttpResponseRedirect('/join_nonprofits/thanks/')
-        else:
-            form = forms.OrganizationForm()
-    except:
-        raise Http404()
+            utils.send_html_mail('org_mail.html', context, 
+                                 "Thanks for registering your organization !", 
+                                 'noreply@freicoin.org', org.email)
+
+            return HttpResponseRedirect('/join_nonprofits/thanks/')
+    else:
+        form = forms.OrganizationForm()
 
     return render(request, 'new_organiation.html', {'form': form})
 
--- foundation/utils.py
+++ foundation/utils.py
@@ -1,3 +1,14 @@
+from django.core.mail import EmailMessage
+from django.template.loader import get_template
+from django.template import Context
+
+def send_html_mail(template, context, subject, from_mail, to_mails):
+    
+    mail_template = get_template(template)
+    mail_content = mail_template.render(Context(context))
+    msg = EmailMessage(subject, mail_content, from_mail, to_mails.split(','))
+    msg.content_subtype = "html"  # Main content is now text/html
+    msg.send()
 
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
