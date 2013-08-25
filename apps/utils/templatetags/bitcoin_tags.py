a625c6e44683b545c69f5e29c9e8d0339a7d3f22
jtimon <jtimon@monetize.io>
2013-08-25 23:34:45 +0200

    Refactor django filters

--- apps/utils/templatetags/bitcoin_tags.py
+++ apps/utils/templatetags/bitcoin_tags.py
@@ -4,26 +4,24 @@ from django.conf import settings
 
 register = template.Library()
 
+def explorer_link(explorer, element, value):
+    return '<a href="%(explorer)s/%(element)s/%(value)s">%(value)s</a>' % {
+        'explorer': explorer,
+        'element': element,
+        'value': value}
+
 @register.filter
 def frc_address(value):
-    return '<a href="%(explorer)s/address/%(address)s">%(address)s</a>' % {
-        'explorer': settings.FRC_EXPLORER,
-        'address': value}
+    return explorer_link(settings.FRC_EXPLORER, "address", value)
 
 @register.filter
 def btc_address(value):
-    return '<a href="%(explorer)s/address/%(address)s">%(address)s</a>' % {
-        'explorer': settings.BTC_EXPLORER,
-        'address': value}
+    return explorer_link(settings.BTC_EXPLORER, "address", value)
 
 @register.filter
 def frc_tx(value):
-    return '<a href="%(explorer)s/tx/%(address)s">%(address)s</a>' % {
-        'explorer': settings.FRC_EXPLORER,
-        'address': value}
+    return explorer_link(settings.FRC_EXPLORER, "tx", value)
 
 @register.filter
 def btc_tx(value):
-    return '<a href="%(explorer)s/tx/%(address)s">%(address)s</a>' % {
-        'explorer': settings.BTC_EXPLORER,
-        'address': value}
+    return explorer_link(settings.BTC_EXPLORER, "tx", value)
