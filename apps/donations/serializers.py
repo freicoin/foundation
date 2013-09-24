13a9d5efd86d46d4580f2da464d1d911e44dbf9d
jtimon <jtimon@monetize.io>
2013-09-24 13:56:41 +0200

    Fix addresses for the detail

--- apps/donations/serializers.py
+++ apps/donations/serializers.py
@@ -7,11 +7,14 @@ class OrganizationSerializer(serializers.ModelSerializer):
     owner = serializers.Field(source='user.username')
     validated_by = serializers.Field(source='validated_by.username')
     validated = serializers.Field(source='validated')
+    foundation_address = serializers.Field(source='foundation_address_value')
+    freicoin_address = serializers.Field(source='freicoin_address_value')
 
     class Meta:
         model = Organization
         fields = ('id', 'name', 'website', 'category', 'category_name', 'user', 'owner',
-                  'short_description', 'long_description', 'validated', 'validated_by')
+                  'short_description', 'long_description', 'validated', 'validated_by',
+                  'foundation_address', 'freicoin_address')
 
 class OrganizationShortSerializer(serializers.ModelSerializer):
     category_name = serializers.Field(source='category.name')
