f35ed250e3f88ce64f0b5898a17f7aa1226ce101
jtimon <jtimon@monetize.io>
2013-10-10 18:34:12 +0200

    Don't parse urls to static resources

--- static/js/app.js
+++ static/js/app.js
@@ -4,28 +4,32 @@ var tradeApp = angular.module('foundationApp', ['django_constants', 'navCtrls', 
                                                 'donationsCtrls', 'donationsSrvs']).
   config(function($routeProvider, django) {
     $routeProvider.
-      when('/', {templateUrl: django.static_urls.home}).
-      when('/about', {templateUrl: django.static_urls.about}).
-      when('/copyright', {templateUrl: django.static_urls.copyright}).
-      when('/register', {templateUrl: django.static_urls.register, controller: 'RegisterCtrl'}).
+      when('/', {templateUrl: 'static/html/home.html'}).
+      when('/about', {templateUrl: 'static/html/about.html'}).
+      when('/copyright', {templateUrl: 'static/html/copyright.html'}).
+      when('/register', {templateUrl: 'static/html/register_form.html', 
+                         controller: 'RegisterCtrl'}).
 
-      when('/trade', {templateUrl: django.static_urls.trade_list, controller: 'MerCategoriesCtrl'}).
-      when('/trade/join', {templateUrl: django.static_urls.trade_form, controller: 'MerchantEditCtrl'}).
-      when('/trade/edit/:merchantId', {templateUrl: django.static_urls.trade_form, 
+      when('/trade', {templateUrl: 'static/html/trade/merchant_list.html', 
+                      controller: 'MerCategoriesCtrl'}).
+      when('/trade/join', {templateUrl: 'static/html/trade/merchant_form.html', 
+                           controller: 'MerchantEditCtrl'}).
+      when('/trade/edit/:merchantId', {templateUrl: 'static/html/trade/merchant_form.html', 
                                        controller: 'MerchantEditCtrl'}).
-      when('/trade/detail/:merchantId', {templateUrl: django.static_urls.trade_detail, 
+      when('/trade/detail/:merchantId', {templateUrl: 'static/html/trade/merchant_detail.html', 
                                          controller: 'MerchantDetailCtrl'}).
-      when('/trade/:merchantType', {templateUrl: django.static_urls.trade_list, 
+      when('/trade/:merchantType', {templateUrl: 'static/html/trade/merchant_list.html', 
                                     controller: 'MerCategoriesCtrl'}).
 
-      when('/donations', {templateUrl: django.static_urls.donations_list, 
+      when('/donations', {templateUrl: 'static/html/donations/organization_list.html', 
                           controller: 'OrgCategoriesCtrl'}).
-      when('/donations/join', {templateUrl: django.static_urls.donations_form, controller: 'OrgEditCtrl'}).
-      when('/donations/edit/:orgId', {templateUrl: django.static_urls.donations_form, 
+      when('/donations/join', {templateUrl: 'static/html/donations/organization_form.html', 
+                               controller: 'OrgEditCtrl'}).
+      when('/donations/edit/:orgId', {templateUrl: 'static/html/donations/organization_form.html', 
                                        controller: 'OrgEditCtrl'}).
-      when('/donations/detail/:orgId', {templateUrl: django.static_urls.donations_detail, 
-                              controller: 'OrgDetailCtrl'}).
-      when('/donations/:orgType', {templateUrl: django.static_urls.donations_list, 
+      when('/donations/detail/:orgId', {templateUrl: 'static/html/donations/organization_detail.html', 
+                                        controller: 'OrgDetailCtrl'}).
+      when('/donations/:orgType', {templateUrl: 'static/html/donations/organization_list.html', 
                                    controller: 'OrgCategoriesCtrl'}).
 
       otherwise({redirectTo: '/'});
