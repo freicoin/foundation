762a53ea063cb230f07914c7de5b3fbde2fd8b7c
Mark Friedenbach <mark@monetize.io>
2013-10-31 11:16:41 -0700

    Specify ALLOWED_HOSTS.

--- foundation/settings/common.py
+++ foundation/settings/common.py
@@ -18,7 +18,7 @@ MANAGERS = ADMINS
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['foundation.herokuapp.com', '.freicoin.org']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
